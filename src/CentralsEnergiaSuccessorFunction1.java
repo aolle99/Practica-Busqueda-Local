33aff9e0d9f6bffaf60bedaef2c4d651f57edb66
Cristina <77931477+crismigo@users.noreply.github.com>
2022-10-04 20:07:06 +0200

    Update CentralsEnergiaSuccessorFunction1.java

--- src/CentralsEnergiaSuccessorFunction1.java
+++ src/CentralsEnergiaSuccessorFunction1.java
@@ -1,17 +1,26 @@
+import IA.Energia.Central;
+import IA.Energia.Cliente;
 import aima.search.eightpuzzle.EightPuzzleBoard;
 import aima.search.framework.SuccessorFunction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 
 public class CentralsEnergiaSuccessorFunction1 implements SuccessorFunction {
 
     public List getSuccessors(Object state) {
+        Set<Integer> consumidorsZero;
+        ArrayList<Central> centrals;
+        ArrayList<Cliente> clients;
+        ArrayList successors = new ArrayList();
+        CentralsEnergiaBoard board = (CentralsEnergiaBoard) state;
 
-        List successors = new ArrayList();
-
+        //Treure un consumidor amb prioritat no garantizada d’una central
+        //for (int i = 0; i < )
         return successors;
+
     }
 
     private void copyOf(EightPuzzleBoard board) {
