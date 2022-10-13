2c9459640d495e55ae0dda063c2907f76e87476d
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-10-13 21:36:42 +0200

    arreglat el goal test per a que ni faci la copia

--- src/CentralsEnergiaGoalTest.java
+++ src/CentralsEnergiaGoalTest.java
@@ -3,8 +3,6 @@ import aima.search.framework.GoalTest;
 
 public class CentralsEnergiaGoalTest implements GoalTest {
     public boolean isGoalState(Object state) {
-        CentralsEnergiaBoard board = (CentralsEnergiaBoard) state;
-        //return board.isGoal();
         return true;
     }
 
